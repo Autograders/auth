3606971539b21a915ecc70cbd49dbe7833ebbdff
Andrés Castellanos <andres.cv@galileo.edu>
2021-01-28 18:29:58 -0600

    update application entry point

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,23 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import fs from 'fs';
 import cors from 'cors';
+import http from 'http';
+import https from 'https';
+import morgan from 'morgan';
 import express from 'express';
 import mongoose from 'mongoose';
+import stoppable from 'stoppable';
 import constants from './constants';
 import compresion from 'compression';
 import cookieParser from 'cookie-parser';
 
-import { auth } from './api';
+import { StoppableServer } from 'stoppable';
+import { auth, aws, task, submit } from './api';
+
+/** HTTP/S server */
+let server: StoppableServer;
 
 /**
  * Creates a DB connection.
@@ -17,16 +26,37 @@ import { auth } from './api';
 function createConnection(): Promise<boolean> {
   return new Promise((resolve, reject) => {
     console.log('Connecting to DB...');
-    mongoose.connect(constants.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.connect(constants.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
     mongoose.connection.once('open', () => {
       console.log('Connected to DB');
       resolve(true);
     });
-    mongoose.connection.on('error', () => {
-      console.error('Could not connect to DB');
+    mongoose.connection.on('error', (error) => {
+      constants.LOGGER.fatal('Could not connect to DB');
+      constants.LOGGER.fatal(error);
       reject(false);
     });
   });
+}
+
+/**
+ * Gets HTTP/S server.
+ *
+ * @param app - ExpressJS application
+ */
+function getServer(app: express.Express): StoppableServer {
+  if (constants.IS_PROD) {
+    return stoppable(
+      https.createServer(
+        {
+          key: fs.readFileSync(constants.APP_SSL_KEY, 'utf-8'),
+          cert: fs.readFileSync(constants.APP_SSL_CRT, 'utf-8')
+        },
+        app
+      )
+    );
+  }
+  return stoppable(http.createServer(app));
 }
 
 /**
@@ -43,16 +73,44 @@ async function start() {
     app.use(compresion());
     app.use(cookieParser());
     app.use(express.json());
+    app.use(express.text());
+    app.use(morgan('combined'));
     // add API to Express app
+    app.use('/aws', aws);
     app.use('/auth', auth);
-    // start
-    app.listen(constants.APP_PORT, () => {
-      console.log(`App listening on port ${constants.APP_PORT}...`);
-    });
+    app.use('/task', task);
+    app.use('/submit', submit);
+    // create http server
+    server = getServer(app);
+    // start server
+    console.log('Starting application...');
+    server.listen(constants.APP_PORT, () => console.log('Application running...'));
   } catch (error) {
-    console.error('Could not start application');
+    constants.LOGGER.fatal('Could not start application');
+    constants.LOGGER.fatal(error);
     process.exit(1);
   }
 }
+
+/**
+ * Shutdowns application.
+ */
+function shutdown() {
+  if (server) {
+    server.stop((error) => {
+      if (error) {
+        constants.LOGGER.fatal('Could not stop HTTP server');
+        constants.LOGGER.fatal(error);
+        process.exit(1);
+      }
+      process.exit(0);
+    });
+  } else {
+    process.exit(0);
+  }
+}
+
+process.on('SIGINT', () => shutdown());
+process.on('SIGTERM', () => shutdown());
 
 start();
