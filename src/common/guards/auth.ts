568332b19b4e3f7f9e20cd1e67d5722d44bd5cff
Andrés Castellanos <andres.cv@galileo.edu>
2021-02-25 16:41:56 -0600

    fix admin guard

--- src/common/guards/auth.ts
+++ src/common/guards/auth.ts
@@ -1,4 +1,3 @@
-import { IUser } from '@models/user';
 import { Forbidden } from '@common/exceptions';
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 
@@ -9,8 +8,7 @@ import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 export class AdminGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const req = context.switchToHttp().getRequest();
-    const user = req.body.user as IUser;
-    if (!user.admin) throw new Forbidden();
+    if (!req.user.admin) throw new Forbidden();
     return true;
   }
 }
