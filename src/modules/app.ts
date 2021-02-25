f26794a877d68aad4badd148ffd87f9b89a1a83a
Andrés Castellanos <andres.cv@galileo.edu>
2021-02-25 11:53:04 -0600

    remove PinModule from UserModule imports

--- src/modules/app.ts
+++ src/modules/app.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PinModule } from './pin/module';
 import { AuthModule } from './auth/module';
 import { UserModule } from '@modules/user/module';
 import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), UserModule, AuthModule]
+  imports: [ScheduleModule.forRoot(), AuthModule, PinModule, UserModule]
 })
 export class AppModule {}
--- src/modules/user/module.ts
+++ src/modules/user/module.ts
@@ -1,10 +1,8 @@
 import { UserService } from './service';
 import { Module } from '@nestjs/common';
 import { UserController } from './controller';
-import { PinModule } from '@modules/pin/module';
 
 @Module({
-  imports: [PinModule],
   controllers: [UserController],
   providers: [UserService]
 })
